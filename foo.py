9b8f1463ea647db7eb380ee20e0ba1ac9d28ae7d
charalambosp <papaloizouc@gmail.com>
2013-09-22 22:37:59 +0100

    tidy up a bit

--- foo.py
+++ foo.py
@@ -2,11 +2,12 @@ __author__ = 'foobar'
 from xlrd import open_workbook
 from xlrd.sheet import Sheet
 
+
 class SheetReader(object):
     state_changes = {
         "schema": "table",
         "table": "cols",
-        "cols":"rows",
+        "cols": "rows",
         "rows": "table"
     }
 
@@ -27,9 +28,10 @@ class SheetReader(object):
 
     def changes_state(f):
         def wrapper(*args):
-            ret =  f(*args)
+            ret = f(*args)
             args[0].state_process()
             return ret
+
         return wrapper
 
     def process_sheet(self, sheet:Sheet):
@@ -43,13 +45,13 @@ class SheetReader(object):
     def state_process(self):
         self.state = self.state_changes[self.state]
 
-    def fix_floats(self,values):
+    def fix_floats(self, values):
         return [self.fix_float(value) for value in values]
 
-    def fix_float(self,value):
-        if isinstance(value,str):
+    def fix_float(self, value):
+        if isinstance(value, str):
             return value
-        elif isinstance(value,float):
+        elif isinstance(value, float):
             str_value = str(value)
             if str_value.endswith(".0"):
                 return str(int(value))
@@ -57,10 +59,10 @@ class SheetReader(object):
                 return str_value
 
 
-    def process_row(self,row_index:int, sheet:Sheet):
+    def process_row(self, row_index:int, sheet:Sheet):
         values = sheet.row_values(row_index)
         values = self.fix_floats(values)
-        if values == ['']*len(values):
+        if values == [''] * len(values):
             self.add_table()
             self.state_process()
         else:
@@ -68,12 +70,12 @@ class SheetReader(object):
 
     @changes_state
     def process_schema(self, row_index:int, sheet:Sheet):
-        self.schema = sheet.cell(row_index,0).value
+        self.schema = sheet.cell(row_index, 0).value
 
     @changes_state
     def process_table(self, row_index:int, sheet:Sheet):
-        self.current_table_name = sheet.cell(row_index,0).value
-        self.current_table = Table(self.schema,self.current_table_name,[],[])
+        self.current_table_name = sheet.cell(row_index, 0).value
+        self.current_table = Table(self.schema, self.current_table_name, [], [])
 
     @changes_state
     def process_cols(self, row_index:int, sheet:Sheet):
@@ -87,13 +89,13 @@ class SheetReader(object):
 
 
 class Table:
-    def __init__(self,schema:str,table:str,cols:list,rows:list):
+    def __init__(self, schema:str, table:str, cols:list, rows:list):
         self.schema = schema
         self.table = table
         self.cols = cols
         self.rows = rows
         self.max_dict = {}
-        self.extra_column_spaces = "  "
+        self.extra_column_spaces = " "
 
     def add_row(self, row):
         self.rows.append(["NULL" if not i else i for i in row])
@@ -101,37 +103,45 @@ class Table:
     def max_size_for_cols(self):
         #black magic :*(
         values_by_col = [
-            (col_index,row[col_index])
+            (col_index, row[col_index])
             for row in self.rows + [self.cols]
-            for col_index in range(0,len(self.cols))
+            for col_index in range(0, len(self.cols))
         ]
-        for col_index in range(0,len(self.cols)):
-            filtered_by_col = filter(lambda arg: arg[0]==col_index,values_by_col)
-            max_tuple = max(filtered_by_col,key=lambda l:len(l[1]))
+        for col_index in range(0, len(self.cols)):
+            filtered_by_col = filter(lambda arg: arg[0] == col_index, values_by_col)
+            max_tuple = max(filtered_by_col, key=lambda l: len(l[1]))
             self.max_dict[max_tuple[0]] = len(max_tuple[1] + self.extra_column_spaces)
 
 
-    def row_to_str(self,row):
-        ret_str = "".join(["'"+str(k) + "'" + "".join((self.max_dict[i]-len(k))*[" "]) +"," for i,k in enumerate(row)])[:-1]
-        return "\n(%s)\n" % ret_str
+    def row_to_str(self, row):
+        #black magic :*(
+        ret_str = "".join(
+            ["'" + str(k) + "'" +
+             "".join(
+                 (self.max_dict[i] - len(k)) * [" "]
+             ) + "," for i, k in enumerate(row)
+            ]
+        )[:-1]
+        return "(%s)\n" % ret_str
 
 
     def __str__(self):
         self.max_size_for_cols()
-        insert = "INSERT INTO `%s`.`%s`" % (self.schema,self.table)
-        columns = self.row_to_str(self.cols)
-        values= "\nVALUES\n"
-        rows = "".join([self.row_to_str(row) for row in self.rows])
-        print(insert)
-        print(columns)
-        print(values)
-        print(rows)
+        insert = "INSERT INTO `%s`.`%s`" % (self.schema, self.table)
+        columns = "\n" + self.row_to_str(self.cols)
+        values = "VALUES"
+        rows = "\n" + "".join([self.row_to_str(row) for row in self.rows])
+        print(insert, end="")
+        print(columns, end="")
+        print(values, end="")
+        print(rows, end="")
         self.max_size_for_cols()
 
-        return "%s %s %s %s" % (self.schema,self.table,self.cols,self.rows)
+        return "%s %s %s %s" % (self.schema, self.table, self.cols, self.rows)
 
     def __repr__(self):
-        return "%s %s %s %s" % (self.schema,self.table,self.cols,self.rows)
+        return "%s %s %s %s" % (self.schema, self.table, self.cols, self.rows)
+
 
 if __name__ == "__main__":
     book = open_workbook('foo.xls')
